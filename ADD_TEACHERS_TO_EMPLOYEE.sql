f7c1171b530d57fe9caf28c97c7a81a1798faa11
phuct <phuct@jonh_shelby>
2026-03-11 10:17:31 +0700

    phuc/ them syllabus vao ui

--- ADD_TEACHERS_TO_EMPLOYEE.sql
+++ ADD_TEACHERS_TO_EMPLOYEE.sql
@@ -4,19 +4,19 @@
 USE [LanguageCenterDB]
 GO
 
--- Add Nguyễn Thị Thu Lan (UserID = 4) to Employee table
+-- Add Nguyen Thi Thu Lan (UserID = 4) to Employee table
 -- She is already a teacher (RoleID = 4) but missing from Employee
 INSERT INTO [dbo].[Employee] ([EmployeeID], [HireDate], [Education], [Experience])
 VALUES (4, CAST(N'2026-01-15' AS Date), N'IELTS 8.0', N'Experienced English teacher with 5+ years teaching TOEIC and IELTS preparation courses')
 GO
 
--- Update Lê Nhựt Huy (UserID = 9) from Admin (RoleID = 1) to Teacher (RoleID = 4)
+-- Update Le Nhut Huy (UserID = 9) from Admin (RoleID = 1) to Teacher (RoleID = 4)
 UPDATE [dbo].[User]
 SET [RoleID] = 4, [Status] = 1
 WHERE [UserID] = 9
 GO
 
--- Add Lê Nhựt Huy (UserID = 9) to Employee table
+-- Add Le Nhut Huy (UserID = 9) to Employee table
 INSERT INTO [dbo].[Employee] ([EmployeeID], [HireDate], [Education], [Experience])
 VALUES (9, CAST(N'2026-03-01' AS Date), N'IELTS 6.5', N'Passionate English teacher with expertise in communicative language teaching methods')
 GO
@@ -30,7 +30,6 @@ WHERE u.UserID IN (4, 9)
 GO
 
 PRINT 'Successfully added teachers to Employee table!'
-PRINT 'UserID 4: Nguyễn Thị Thu Lan - Already a teacher, now in Employee'
-PRINT 'UserID 9: Lê Nhựt Huy - Changed from Admin to Teacher, added to Employee'
+PRINT 'UserID 4: Nguyen Thi Thu Lan - Already a teacher, now in Employee'
+PRINT 'UserID 9: Le Nhut Huy - Changed from Admin to Teacher, added to Employee'
 GO
-
